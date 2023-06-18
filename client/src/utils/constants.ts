d4a0a3f15abfc717d526de9ae396befeccc879fa
vlados <vladosik4891@gmail.com>
2023-06-18 15:27:08 +0300

    HREF fix

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -16,7 +16,7 @@ export const MENU_LIST = [
 export const HREF
     = window.location.hostname === 'localhost'
     	? 'http://localhost:5001/'
-    	: 'https://vmestateplovdiv.com';
+    	: 'https://vmestateplovdiv.com/';
 
 
 export const PostField = [
